c5076c58ea865332aeeebe0988ae9c5d8146ca89
cubedurubik <42170320+cubedurubik@users.noreply.github.com>
2022-12-01 23:42:12 +0100

    pleins de trucs

--- js/Entites.js
+++ js/Entites.js
@@ -1,5 +1,6 @@
 function Entite(name) {
     this.name = name;
+    this.class = 'Entite';
     this.health = generateParameters(name).health;
     this.attack = generateParameters(name).attack;
     this.defense = generateParameters(name).defense;
@@ -18,13 +19,15 @@ function generateParameters(name) {
     }
     switch (name) {
         case 'Xx_Hepatite_xX':
-            parameters.health = 100;
+            parameters.class = 'MST';
+            parameters.health = 20;
             parameters.attack = 1;
             parameters.defense = 1;
             parameters.speed = 1;
             parameters.image = 'assets/images/entites/virus.png';
             break;
         case 'Turbo VIH':
+            parameters.class = 'MST';
             parameters.health = 10;
             parameters.attack = 10;
             parameters.defense = 10;
@@ -32,6 +35,7 @@ function generateParameters(name) {
             parameters.image = 'assets/images/entites/virus.png';
             break;
         case 'Mega Sida':
+            parameters.class = 'MST';
             parameters.health = 10;
             parameters.attack = 10;
             parameters.defense = 10;
@@ -39,6 +43,7 @@ function generateParameters(name) {
             parameters.image = 'assets/images/entites/virus.png';
             break;
         case 'Antibiotique':
+            parameters.class = 'Medicament';
             parameters.health = 500;
             parameters.attack = 5;
             parameters.defense = 5;
@@ -46,6 +51,7 @@ function generateParameters(name) {
             parameters.image = 'assets/images/entites/medic.png';
             break;
         case 'Preservatif':
+            parameters.class = 'Medicament';
             parameters.health = 1;
             parameters.attack = 9999;
             parameters.defense = 9999;
@@ -53,6 +59,7 @@ function generateParameters(name) {
             parameters.image = 'assets/images/entites/medic.png';
             break;
         case 'Contraception':
+            parameters.class = 'Medicament';
             parameters.health = 100;
             parameters.attack = 10;
             parameters.defense = 10;
--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
-    console.log('Hello World!');
 
-    function debugDiscordBot() {
-        console.log('Discord bot is ready!');
-        return 'Discord bot is ready!';
-    }
+    $('.ui.dropdown')
+    .dropdown()
+    ;
+
+    let mst = $('#' + $('#MST').val()).text();
+    let medic = $('#' + $('#medicament').val()).text();
+    let MST = new Entite(mst);
+    let medicament = new Entite(medic);
+    updateFightInfos(MST, medicament);
+    generateModels(MST, medicament)
+
+
 
     $('#startButton').click(function () {
         let mst = $('#' + $('#MST').val()).text();
@@ -13,78 +20,17 @@ $(document).ready(function () {
         
     });
 
-    function startFight(mst, medic) {
+    $('#MST, #medicament').change(function () {
+        let mst = $('#' + $('#MST').val()).text();
+        let medic = $('#' + $('#medicament').val()).text();
         let MST = new Entite(mst);
         let medicament = new Entite(medic);
         updateFightInfos(MST, medicament);
-        generateModels(MST, medicament);
-        playFight(MST, medicament);
-        }
+        generateModels(MST, medicament)
+    });
 
-
-    // wait 1 second then print the fight infos in console
-    function wait(ms) {
-        setTimeout(function () {
-            console.log('MST: ');
-        }, ms);
-
-
-    function playFight(MST, medicament) {
-        while (MST.isAlive && medicament.isAlive) {
-            updateFightInfos(MST, medicament);
-                if (MST.speed > medicament.speed) {
-                    attack(MST, medicament);
-                    if (medicament.isAlive) {
-                        attack(medicament, MST);
-                    }
-                } else {
-                    attack(medicament, MST);
-                    if (MST.isAlive) {
-                        attack(MST, medicament);
-                    }
-                }
-        }
-        if (MST.isAlive) {
-            console.log(MST.name + ' a gagné !');
-            $('#winner').text(MST.name);
-        } else {
-            $('#winner').text(medicament.name);
-        }
-    }
-
-    function attack(attacker, defender) {
-        let damage = attacker.attack - defender.defense;
-        if (damage < 0) {
-            damage = 0;
-        }
-        defender.health -= damage;
-        if (defender.health <= 0) {
-            defender.isAlive = false;
-        }
-    }
-
-
-    function updateFightInfos(MST, medicament) {
-        $('#MSTname').text(MST.name);
-        $('#medicname').text(medicament.name);
-        $('#MST1Health').text(MST.health);
-        $('#medicament1Health').text(medicament.health);
-        $('#MST1Attack').text(MST.attack);
-        $('#medicament1Attack').text(medicament.attack);
-        $('#MST1Defense').text(MST.defense);
-        $('#medicament1Defense').text(medicament.defense);
-        $('#MST1Speed').text(MST.speed);
-        $('#medicament1Speed').text(medicament.speed);
-    }
-
-    function generateModels(MST, medicament) {
-        $('#CampMst').text('');
-        $('#CampMst').append('<img src="' + MST.image + '" alt="' + MST.name + '">');
-        $('#CampMedic').text('');
-        $('#CampMedic').append('<img src="' + medicament.image + '" alt="' + medicament.name + '">');
-        $('#MST1').attr('src', MST.image);
-        $('#medicament1').attr('src', medicament.image);
-    }
+    
+    
 
 
 });
