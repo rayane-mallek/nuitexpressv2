f1ee3b4c072ceaf571fae1c61cb0e13b0eab808f
mallekr <mallekr@Kx2go.dep-info.iutmontp.univ-montp2.fr>
2022-12-01 20:06:53 +0100

    wip shepherdjs

--- js/formulaires.js
+++ js/formulaires.js
@@ -1,1 +1,46 @@
-import { Shepherd } from 'shepherd.js';
+const tour = new Shepherd.Tour({
+    useModalOverlay: true,
+    defaultStepOptions: {
+      classes: 'shadow-md bg-purple-dark',
+      scrollTo: true
+    },S
+});
+
+document.querySelector('.step').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        tour.next
+    }
+});
+
+
+tour.addStep({
+    id: 'virus-name',
+    text: 'Veillez entrer un nom pour le virus.',
+    attachTo: {
+      element: '#name_virus',
+      on: 'bottom'
+    },
+    buttons: [
+      {
+        text: 'Next',
+        action: tour.next
+      }
+    ]
+});
+
+tour.addStep({
+    id: 'medicament-name',
+    text: 'Veillez entrer un nom pour le médicament.',
+    attachTo: {
+      element: '#name_medicament',
+      on: 'bottom'
+    },
+    buttons: [
+      {
+        text: 'Next',
+        action: tour.next
+      }
+    ]
+});
+
+tour.start();
